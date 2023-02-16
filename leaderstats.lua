1011d9458821e634971a20cc23b2944e76941e58
htmlnine <121134039+htmlnine@users.noreply.github.com>
2023-02-15 19:42:38 -0800

    Update leaderstats.lua

--- leaderstats.lua
+++ leaderstats.lua
@@ -1,4 +1,29 @@
+--adafa ralph 14/2/23
+
 game.Players.PlayerAdded:Connect(function(player)
 	local leaderstats = Instance.new("Folder",player)
-	leaderstats.Name = "Leaderstats"
+	leaderstats.Name = "leaderstats"
+	
+	local Coins = Instance.new("IntValue",leaderstats)
+	Coins.Name = "Coins"
+	Coins.Value = 0
+	
+	local DataStoreService = game:GetService("DataStoreService")
+	local CoinsDataStore = DataStoreService:GetDataStore("Coins")
+	
+	Coins.Value = CoinsDataStore:GetAsync(player.UserId,Coins.Value)
+	print("loaded coin data for "..player.Name)
+end)
+
+
+game.Players.PlayerRemoving:Connect(function(player)
+	local leaderstats = player.leaderstats
+	local Coins = leaderstats.Coins
+	local DataStoreService = game:GetService("DataStoreService")
+	local CoinsDataStore = DataStoreService:GetDataStore("Coins")
+	local sucess , errormessage = pcall(function()
+		CoinsDataStore:SetAsync(player.UserId,Coins.Value)
+		print("saved coin data for "..player.Name)
+		print(Coins.Value)
+	end)
 end)
